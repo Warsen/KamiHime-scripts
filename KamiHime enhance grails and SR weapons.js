767ddb449f4aa99edbdc0f7a8d2eb2a65354d199
Warsen <elias.p.carrillo@gmail.com>
2018-05-27 01:53:27 -0700

    Rewrote grail and SR enhancement script. Only one list request with no recursion. SR grails get to 4, SR weapons get to 2.

--- KamiHime enhance grails and SR weapons.js
+++ KamiHime enhance grails and SR weapons.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         KamiHime enhance grails and SR weapons
+// @name         KamiHime Auto Enhance Grails and SR Weapons
 // @namespace    http://tampermonkey.net/
 // @version      24.05.2018
-// @description  enhance SR weapons to 4 skill level, R grails to 3 skill level, SR grails to 5 skill level
+// @description  Enhances R grails to skill level 3, SR grails to skill level 4, and SR weapons to skill level 2
 // @author       You
 // @include      https://cf.r.kamihimeproject.dmmgames.com/front/cocos2d-proj/components-pc/mypage_quest_party_guild_enh_evo_gacha_present_shop_epi/app.html*
 // @include      https://cf.g.kamihimeproject.dmmgames.com/front/cocos2d-proj/components-pc/mypage_quest_party_guild_enh_evo_gacha_present_shop_epi/app.html*
@@ -10,86 +10,123 @@
 // @run-at       document-end
 // ==/UserScript==
 
-//script uses unlocked SR weapons and enhance them to 4 skill level
+async function khLoadingAsync()
+{
+	// Wait for the game to finish loading
+	while (!has(cc, "director", "_runningScene", "_seekWidgetByName") && !has(kh, "createInstance"))
+	{
+		await new Promise(resolve => setTimeout(resolve, 1000));
+	}
 
-function start(){
-	if (has(cc, "director", "_runningScene", "_seekWidgetByName") && has(kh, "createInstance")){
-			setTimeout(getWeaponInfo,1000);
-	} else {
-		console.log('waiting for page');
-		setTimeout(start,2000);
+	await getWeaponInfo();
+}
+
+async function getWeaponInfo()
+{
+	let fodder = [];
+	let srWeapons = [];
+	let srGrails = [];
+	let rGrails = [];
+
+	console.log("Starting auto foddering script");
+
+	let result = await kh.createInstance("apiAWeapons").getList(0, 500);
+
+	console.log("Found " + result.body.data.length + " weapons");
+
+	result.body.data.forEach(function(item) {
+		if (item.rare === "R" && item.bonus === 0 && item.level === 1 && !item.is_equipped && !item.is_locked && item.attack > 100)
+		{
+			fodder.push(item.a_weapon_id);
+		}
+		else if (item.rare === "R" && item.bonus === 0 && item.level < 8 && !item.is_equipped && !item.is_locked && item.name === "Arcane Grail")
+		{
+			rGrails.push(item.a_weapon_id);
+		}
+		else if (item.rare === "SR" && item.bonus === 0 && item.level < 15 && !item.is_equipped && !item.is_locked && item.name === "False Grail Yaldabaoth")
+		{
+			srGrails.push(item.a_weapon_id);
+		}
+		else if (item.rare === "SR" && item.bonus === 0 && item.level === 1 && !item.is_equipped && !item.is_locked && item.attack > 150)
+		{
+			srWeapons.push(item.a_weapon_id);
+		}
+	});
+
+	// Reorder these loops to change priority
+
+	while (rGrails.length > 0 && fodder.length >= 2)
+	{
+		let result = await kh.createInstance("apiAWeapons").get(rGrails.shift());
+		checkAndEnhanceWeapon(result.body, fodder);
+	}
+	while (srGrails.length > 0 && fodder.length >= 6)
+	{
+		let result = await kh.createInstance("apiAWeapons").get(srGrails.shift());
+		checkAndEnhanceWeapon(result.body, fodder);
+	}
+	while (srWeapons.length > 0 && fodder.length >= 1)
+	{
+		let result = await kh.createInstance("apiAWeapons").get(srGrails.shift());
+		checkAndEnhanceWeapon(result.body, fodder);
+	}
+
+	console.log("Completed auto foddering script");
+}
+
+async function checkAndEnhanceWeapon(weapon, fodder)
+{
+	let useWeapons = [];
+	let targetSkillLevel = 2;
+
+	if (weapon.name === "Arcane Grail")
+	{
+		targetSkillLevel = 3;
+	}
+	else if (weapon.name === "False Grail Yaldabaoth")
+	{
+		targetSkillLevel = 4;
+	}
+
+	while (weapon.skills[0].level < targetSkillLevel)
+	{
+		if (weapon.name === "Arcane Grail")
+		{
+			useWeapons.push(fodder.pop());
+		}
+		else
+		{
+			for (let i=0; i < weapon.skills[0].level; i++)
+			{
+				useWeapons.push(fodder.pop());
+			}
+		}
+
+		let result = await kh.createInstance("apiAWeapons").enhance(weapon.a_weapon_id, useWeapons);
+
+		weapon = result.body.weapon;
+		useWeapons = [];
 	}
 }
 
-function getWeaponInfo(){
-    kh.createInstance("apiAWeapons").getList(0,500).then(function(e) {prepare(e.body);}.bind(this));
-}
+function has(obj)
+{
+	if (obj !== Object(obj)) return false;
 
-function prepare(weaponInfo){
-    console.log(weaponInfo);
-    var toEnhance = [];
-    var fodder = [];
-    var grailsR = [];
-    weaponInfo.data.forEach(function(item){
-        if (item.rare === "R" && item.bonus === 0 && item.level === 1 && !item.is_equipped && !item.is_locked && item.attack > 100) {
-            fodder.push(item.a_weapon_id);
-        }
-        if (item.rare === "SR" && item.bonus === 0 && item.level < 15 && !item.is_equipped && !item.is_locked && item.attack >= 150) {
-            toEnhance.push(item.a_weapon_id);
-        }
-        if (item.rare === "R" && item.bonus === 0 && item.level < 8 && !item.is_equipped && !item.is_locked && item.name === "Arcane Grail") {
-            grailsR.push(item.a_weapon_id);
-        }
-    });
-    console.log(fodder);
-    console.log(toEnhance);
-
-    if (toEnhance.length > 0 && fodder.length > 5) {
-        kh.createInstance("apiAWeapons").get(toEnhance[0]).then(function(e) {enhance(e.body,fodder);}.bind(this));
-        toEnhance.shift();
-    } else if (grailsR.length > 0 && fodder.length > 2){
-        kh.createInstance("apiAWeapons").get(grailsR[0]).then(function(e) {enhance(e.body,fodder);}.bind(this));
-    } else {
-        console.log("No materials to perform enhance, stop");
-    }
-}
-
-function enhance(weapon,fodder){
-    console.log(weapon);
-    var tempFodder = [];
-    var skillLVL = weapon.skills[0].level;
-    var maxSkillLevel = 4;
-    if (weapon.name === "Arcane Grail") maxSkillLevel = 3;
-    if (weapon.name === "False Grail Yaldabaoth") maxSkillLevel = 5;
-    if (skillLVL < maxSkillLevel) {
-        if (weapon.name === "Arcane Grail") {
-            tempFodder.push(fodder.pop());
-        } else {
-            for (let i=0; i<skillLVL; i++){
-                tempFodder.push(fodder.pop());
-            }
-        }
-        console.log(tempFodder);
-        kh.createInstance("apiAWeapons").enhance(weapon.a_weapon_id,tempFodder).then(function(e) {enhance(e.body.weapon,fodder);}.bind(this));
-    } else {
-        getWeaponInfo();
-    }
-}
-
-function has(obj) {
-	var prop;
-	if (obj !== Object(obj)) {
-		return false;
-	}
-	for (let i = 1; i < arguments.length; i++) {
-		prop = arguments[i];
-		if ((prop in obj) && obj[prop] !== null && obj[prop] !== 'undefined') {
+	for (let i = 1; i < arguments.length; i++)
+	{
+		let prop = arguments[i];
+		if ((prop in obj) && obj[prop] !== null && obj[prop] !== 'undefined')
+		{
 			obj = obj[prop];
-		} else {
+		}
+		else
+		{
 			return false;
 		}
 	}
+
 	return true;
 }
 
-start();
+khLoadingAsync();
